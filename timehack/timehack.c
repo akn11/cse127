8eda327994be4a7bf996a85de6a83d04a1dc9fd7
Ashwin <anambiss@ucsd.edu>
2022-04-28 11:20:20 -0700

    second

--- timehack/timehack.c
+++ timehack/timehack.c
@@ -39,24 +39,26 @@ int main(int argc, char **argv) {
    // guess[3] = 'o';
    // guess[4] = 't';
    // guess[5] = '!';
-  while(!check_pass(guess)) {
+  int times = 0;
+  while(times < 20) {
     bzero(guess, sizeof(guess));
-    long bound = 3850;
+    long bound = 3700;
     for(int i = 0; i < 33; i++) {
     	for(int ascii = 33; ascii < 127; ascii++) {
 	   guess[i] = ascii;
-	   long arr[500];
+	   long arr[300];
 	   for(int z = 0; z < 5; z++){
 		check_pass(guess);
 	   }
-	   for(int j = 0; j < 500; j++) {
+	   for(int j = 0; j < 300; j++) {
 	       long start = rdtsc();
 	       check_pass(guess);
 	       long end = rdtsc();
 	       arr[j] = end - start;
+	       check_pass(guess);
 	   }
-	   for(int x = 0; x < 499; x++) {
-	      for(int y = 0; y < 499; y++) {
+	   for(int x = 0; x < 299; x++) {
+	      for(int y = 0; y < 299; y++) {
 		  if(arr[y] <= arr[y+1]) {
 		     long temp = arr[y];
 		     arr[y] = arr[y+1];
@@ -64,24 +66,31 @@ int main(int argc, char **argv) {
 		  } 
 	      }
 	   }
-	   long median = (arr[249]+arr[250])/2;
-          // printf("%c:  median: %d, bound: %d          ",ascii, median, bound);
+	   long median = (arr[149]+arr[150])/2;
+           //printf("%c:  median: %d, bound: %d          ",ascii, median, bound);
 	  if(median > bound) {
 	      bound = median + 1200;
 	      //bound += 1000;
 	      break;
 	  }
 	}
+	
 	if(check_pass(guess)) {
-	  printf("PASSWORD IS CORRECT\n");
+//	  printf("PASSWORD IS CORRECT\n");
 	  break;
 	}
-	else{
-	    printf("wrong: %s \n", guess); 
-	}
+/*	else{
+          printf("wrong: %s \n", guess); 
+	}*/
+
 //	if(i == 3)
 //	   break;
 //	break;
+    }
+    times += 1;
+    if(check_pass(guess)) {
+  //    printf("RIGHT!!\n");
+      break;
     }
   }	
  /* 
